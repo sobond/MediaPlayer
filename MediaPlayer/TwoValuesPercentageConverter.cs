ccecd1a2222ee23eb2dca5546701188aa3eb7abc
Daniel Soboń <sobondaniel@gmail.com>
2019-02-17 13:24:29 +0100

    Dołożenie logiki w statusbarze + fomratowanie na procenty (ilość odtworzonego klipu)

---
+++ MediaPlayer/TwoValuesPercentageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MediaPlayer
+{
+    class TwoValuesPercentageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            double watchedTime = (double)values[0];
+            double totalTime = (double)values[1];
+            return (watchedTime / totalTime);
+ 
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
